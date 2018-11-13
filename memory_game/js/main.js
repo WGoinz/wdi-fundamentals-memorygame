e4bd9a4b4082fc0bec1f4959a43874aa9bfc9312
WGoinz <wgoinz@gmail.com>
2018-11-13 14:13:30 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,33 @@
 //console.log("Up and running!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardThree);
+
+//Establish main variables in memory game
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+//Create variable for first card
+var cardOne = cards[1];
+
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+//Create variable for second card
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+//If statement to check and see if cardsInPlay is 2
+
+if (cardsInPlay.length === 2)
+	if (cardsInPlay[0] === cardsInPlay[1])
+		alert("You found a match");
+	else
+		alert("Sorry, try again");
