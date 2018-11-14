20bc9463907d1ecd107dfb28134d9e08a0981c50
WGoinz <wgoinz@gmail.com>
2018-11-14 15:41:50 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -12,22 +12,53 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-//Create variable for first card
-var cardOne = cards[1];
 
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
 
-//Create variable for second card
-var cardTwo = cards[1];
+//Create checkForMatch function
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} 
+	else {
+		alert("Sorry, try again.");
+		console.log("Sorry, try again.");	
+	}
+};
 
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+//Create flipCard function
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+	};
+};
+
+flipCard(0);
+flipCard(2);
+
+	// if (cardsInPlay.length === 2)
+	// if (cardsInPlay[0] === cardsInPlay[1])
+	// 	alert("You found a match");
+	// else
+	// 	alert("Sorry, try again");
+
+// //Create variable for first card
+// var cardOne = cards[1];
+
+// cardsInPlay.push(cardOne);
+// console.log("User flipped " + cardOne);
+
+// //Create variable for second card
+// var cardTwo = cards[1];
+
+// cardsInPlay.push(cardTwo);
+// console.log("User flipped " + cardTwo);
 
 //If statement to check and see if cardsInPlay is 2
 
-if (cardsInPlay.length === 2)
-	if (cardsInPlay[0] === cardsInPlay[1])
-		alert("You found a match");
-	else
-		alert("Sorry, try again");
+// if (cardsInPlay.length === 2)
+// 	if (cardsInPlay[0] === cardsInPlay[1])
+// 		alert("You found a match");
+// 	else
+// 		alert("Sorry, try again");
